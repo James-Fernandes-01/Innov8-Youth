d0dddc06251345b216e3ec9d994d8d92722cadbf
James F <britishfernandes@gmail.com>
2025-12-27 20:54:22 -0500

    Finished Contact Us section and footer for the website.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,13 @@
-export default function Footer() {
-    return <div></div>;
+import { ArrowUp } from "lucide-react";
+
+export default function Footer(){
+    return(
+        <footer className="bg-gray-800 text-white py-4 mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between">
+            <p className="text-sm">© 2025 Innov8Youth. All rights reserved.</p>
+            <a href="#top" className="flex items-center">
+                <ArrowUp className="h-4 w-4 mr-1" />
+                <span className="text-sm">Back to top</span>
+            </a>
+        </footer>
+    )
 }
--- src/components/Volunteers.jsx
+++ src/components/Volunteers.jsx
@@ -12,8 +12,7 @@ const volunteers = [
         id: 2,
         title: "Co-Founder",
         name: "James Fernandes",
-        description: "Hello! I'm James, a high school Junior that loves everything robotics." +
-        " My goal is to share my experiences in STEM with others and help them grow their skills and confidence in this amazing field.",
+        description: "Hello! I'm James, a high school junior that loves everything about robotics. I’m interested in all aspects involving mechanical, electrical, software, and business components. My goal is to share my experiences in STEM with others and help them grow their skills and confidence in this expanding and increasingly important field.",
         image: "/James_Fernandes.PNG",
         tags: ["FIRST Robotics Competition Team 8592 Mechanical Subteam"],
     },
@@ -33,8 +32,8 @@ export default function Volunteers() {
     return(
         <section id="volunteers" className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20">
             <h2 className="text-3xl font-extrabold text-center">Our Volunteers</h2>
-            <p className="mt-4 text-lg text-center">
-                Meet our volunteers!
+            <p className="mt-4 text-xl text-center font-medium">
+                Meet the Team!
             </p>
 
             <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
